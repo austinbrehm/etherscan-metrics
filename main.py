5809ff2ab2f3ac1887483e2ac0a0cb5d69bf4364
Austin Brehm <austin.brehm@gmail.com>
2023-11-02 17:14:02 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+import datetime
+
+
+metamask_address = '0xeD873463fb62E2B47BE045beA7385d35166A56dE'
+etherscan_api_key = '3SVXH4QK42N8XZN5BY6BDM9687TN1GMA9K'
+
+
+def unix_time():
+    current_time = datetime.datetime.now()
+    timestamp = datetime.datetime.timestamp(current_time)
+    return timestamp
+
+
+def get_balance(address, api_key):
+    api_url = 'https://api.etherscan.io/api' \
+              '?module=account' \
+              '&action=balance' \
+              f'&address={address}' \
+              '&tag=latest' \
+              f'&apikey={api_key}'
+    response = requests.get(api_url).json()
+    balance_eth = round(float(response['result']) * 10**-18, 10)
+    return balance_eth
+
+
+def get_current_block(api_key):
+    timestamp = str(unix_time())
+    api_url = 'https://api.etherscan.io/api' \
+              '?module=block' \
+              '&action=getblocknobytime' \
+              f'&timestamp={timestamp}' \
+              '&closest=before' \
+              f'&apikey={api_key}'
+    response = requests.get(api_url).json()
+    return response
+
+
+def get_total_gas_used():
+    pass
+
+
+print(f'Balance: {get_balance(metamask_address, etherscan_api_key)} eth')
+print(get_current_block(etherscan_api_key))
+
+
+#TODO: fix unix_time calculation
